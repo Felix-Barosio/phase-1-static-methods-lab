7517bda7901fb570236facaef91b928cc328e5b3
Felix <barosiofelix@gmail.com>
2022-09-07 12:27:10 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z-' ]+/g, '')
+  }
+  static titleize(string) {
+    let exclude = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    const wordArr = string.split(" ")
+    return wordArr.map((word, i) => {
+      return exclude.includes(word) && i != 0 ? [word] : word.charAt(0).toUpperCase() + word.slice(1)
+    }).join(' ');
+  }
 }
